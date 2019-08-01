bded12dce74760c6bcabf428649d8b2235e99490
Nick Luparev <nikita.luparev@gmail.com>
2019-08-01 23:53:11 +0500

    identifiers, keywords and integersls

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"github.com/SuperManEver/monkey_lang/token"
 )
 
+var persons = map[string]string{
+	"John": "Doe",
+	"Bill": "Atkinson",
+}
+
 func main() {
 	// str := token.EOF
 
 	fmt.Printf("hello world %s\n", token.LPAREN)
+	fmt.Printf("Person: %s\n", persons["John"])
 }
